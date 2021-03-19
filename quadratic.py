bf97dfa6ffbcf22292095a0284df3e19ee93dcf2
hi101000 <piklenews@gmail.com>
2021-03-19 10:49:48 -0400

    added test.py

--- quadratic.py
+++ quadratic.py
@@ -1,6 +1,7 @@
 import math
 
 def calc(a, b, c):
-	x1=(-b+math.sqrt(b**2-4*ac))/2*a
-	x2=(-b-math.sqrt(b**2-4*ac))/2*a
+	'''calculates all values of x for a given a b and c - answers are for qadratic equations in the following form: ax^2+bx+c=0'''
+	x1=(-b+math.sqrt(b**2-4*a*c))/2*a
+	x2=(-b-math.sqrt(b**2-4*a*c))/2*a
 	return x1, x2
---
+++ test.py
@@ -0,0 +1,7 @@
+import quadratic
+
+a=1
+b=-16
+c=48
+x1, x2=quadratic.calc(a, b, c)
+print(f'x={x1}, x={x2}')
